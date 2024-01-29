b681d0cb1d57e4b9ab3f24d50e05a6e08b6d363d
vitalnodo <vitalnodo@outlook.com>
2024-01-29 18:52:15 +0200

    add serpent

--- src/root.zig
+++ src/root.zig
@@ -2,11 +2,18 @@ const std = @import("std");
 const testing = std.testing;
 
 const twofish_main = @import("ciphers/twofish/main.zig");
+const serpent_main = @import("ciphers/serpent/main.zig");
 pub const ciphers = struct {
     pub const twofish = struct {
         pub const Twofish256 = twofish_main.Twofish256;
         pub const TwofishEncryptCtx = twofish_main.TwofishEncryptCtx;
         pub const TwofishDecryptCtx = twofish_main.TwofishDecryptCtx;
+    };
+    pub const serpent = struct {
+        pub const Serpent128 = serpent_main.Serpent128;
+        pub const Serpent256 = serpent_main.Serpent256;
+        pub const SerpentEncryptCtx = serpent_main.SerpentEncryptCtx;
+        pub const SerpentDecryptCtx = serpent_main.SerpentDecryptCtx;
     };
 };
 
@@ -14,6 +21,11 @@ test {
     _ = ciphers.twofish.Twofish256;
     _ = ciphers.twofish.TwofishEncryptCtx;
     _ = ciphers.twofish.TwofishDecryptCtx;
+
+    _ = ciphers.serpent.Serpent128;
+    _ = ciphers.serpent.Serpent256;
+    _ = ciphers.serpent.SerpentEncryptCtx;
+    _ = ciphers.serpent.SerpentDecryptCtx;
 }
 
 test {
